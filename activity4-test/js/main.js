94adc68a3385822483c72e0ba8ed69d2b3de38df
SiFrancis <franzo08112007@gmail.com>
2023-07-10 12:12:40 +0800

    tttt

--- activity4-test/js/main.js
+++ activity4-test/js/main.js
@@ -6,9 +6,9 @@
         function toggleNavbarMethod() {
             if ($(window).width() > 992) {
                 $('.navbar .dropdown').on('mouseover', function () {
-                    $('.dropdown-toggle', this).trigger('click');
+                    $('.btn-dropdown', this).trigger('click');
                 }).on('mouseout', function () {
-                    $('.dropdown-toggle', this).trigger('click').blur();
+                    $('.btn-dropdown', this).trigger('click').blur();
                 });
             } else {
                 $('.navbar .dropdown').off('mouseover').off('mouseout');
